acc6f6b99915dd163217160abaa57050b6ba42d5
maykonmichel <maykonmichelpalma@gmail.com>
2020-01-29 11:16:12 -0200

    Configured "db"

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import {GraphQLServer} from 'graphql-yoga';
 
 import resolvers from './resolvers';
+import db from './configs/db';
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: params => ({
+    ...params,
+    db,
+  }),
 });
 
 server
